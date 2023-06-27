d2d54a0b6468e9c6f043df2432413be4860977a1
arielfelippi <ariel.felippi@hotmail.com>
2023-06-27 12:21:24 -0300

    ajustes css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,44 +3,6 @@
 @section('content')
     <header>
         <link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
-        <style>
-            .container a.dash-permissoes {
-                color: #ffc107 !important;
-                text-decoration: none;
-            }
-            .container a.dash-permissoes:hover {
-                color: #ffca2b !important;
-                text-decoration: underline;
-            }
-            .container .search-form .btn-limpar {
-                padding: 8px 16px;
-                background-color: #00bee0b9;
-                border: none;
-                border-radius: 4px;
-                color: #fff;
-                font-size: 14px;
-                cursor: pointer;
-                text-decoration: none;
-            }
-            .container .search-form .btn-limpar:hover {
-                background-color:#00bee0b9;
-                text-decoration: none;
-            }
-            .container a.acoes-excluir {
-                padding: 6px 10px;
-                background-color: #f83c03b9;
-                border: none;
-                border-radius: 4px;
-                color: #cac8c8;
-                font-size: 14px;
-                cursor: pointer;
-                text-decoration: none;
-            }
-            .container a.acoes-excluir:hover {
-                color: white !important;
-                text-decoration: none;
-            }
-        </style>
     </header>
     <div class="sidebar">
         <div class="user-info">
@@ -65,79 +27,81 @@
         @if(empty($documentos))
             <p>Nenhum documento encontrado.</p>
         @else
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Documento</th>
-                        <th>Proprietário</th>
-                        <th>Permissões</th>
-                        <th>Conteúdo</th>
-                        <th>Data de Criação</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($documentos as $documento)
-                        @php
-                            $permissoes = str_replace(" |", "", trim($documento->permissoes));
-                            $proprietario = ($documento->usuario_id == Auth::user()->id || $permissoes == 'todas');
-                            $dataCriacao = $documento->criacao ?? $documento->created_at ?? $documento->updated_at;
-                            $dataCriacao = date('d/m/Y H:i', strtotime($dataCriacao));
-                            $conteudo = strip_tags(trim($documento->conteudo)) ?? 'Arquivo';
-                        @endphp
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td>{{ $documento->id }}</td>
-                            <td>
-                                @if($proprietario)
-                                    @if ($documento->extensao == 'txt')
+                            <th>Id</th>
+                            <th>Documento</th>
+                            <th>Proprietário</th>
+                            <th>Permissões</th>
+                            <th>Conteúdo</th>
+                            <th>Data de Criação</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($documentos as $documento)
+                            @php
+                                $permissoes = str_replace(" |", "", trim($documento->permissoes));
+                                $proprietario = ($documento->usuario_id == Auth::user()->id || $permissoes == 'todas');
+                                $dataCriacao = $documento->criacao ?? $documento->created_at ?? $documento->updated_at;
+                                $dataCriacao = date('d/m/Y H:i', strtotime($dataCriacao));
+                                $conteudo = strip_tags(trim($documento->conteudo)) ?? 'Arquivo';
+                            @endphp
+                            <tr>
+                                <td>{{ $documento->id }}</td>
+                                <td>
+                                    @if($proprietario)
+                                        @if ($documento->extensao == 'txt')
+                                            <a class="dash-permissoes" href="{{ route('documentos.conteudo', ['id' => $documento->id]) }}">{{ $documento->nome }}</a>
+                                        @else
+                                            <a class="dash-permissoes" href="/storage/{{ $documento->caminho_arquivo }}" target="_blank">{{ $documento->nome }}</a>
+                                        @endif
+                                    @elseif($permissoes == 'visualizar' && $documento->extensao != 'txt')
+                                        <a class="dash-permissoes" href="/storage/{{ $documento->caminho_arquivo }}" target="_blank">{{ $documento->nome }}</a>
+                                    @elseif($permissoes == 'editar')
+                                        <a class="dash-permissoes" href="{{ route('documentos.conteudo', ['id' => $documento->id]) }}">{{ $documento->nome }}</a>
+                                    @elseif($permissoes == 'excluir' && $documento->extensao != 'txt')
                                         <a class="dash-permissoes" href="{{ route('documentos.conteudo', ['id' => $documento->id]) }}">{{ $documento->nome }}</a>
                                     @else
-                                        <a class="dash-permissoes" href="/storage/{{ $documento->caminho_arquivo }}" target="_blank">{{ $documento->nome }}</a>
+                                        {{ $documento->nome }}
                                     @endif
-                                @elseif($permissoes == 'visualizar' && $documento->extensao != 'txt')
-                                    <a class="dash-permissoes" href="/storage/{{ $documento->caminho_arquivo }}" target="_blank">{{ $documento->nome }}</a>
-                                @elseif($permissoes == 'editar')
-                                    <a class="dash-permissoes" href="{{ route('documentos.conteudo', ['id' => $documento->id]) }}">{{ $documento->nome }}</a>
-                                @elseif($permissoes == 'excluir' && $documento->extensao != 'txt')
-                                    <a class="dash-permissoes" href="{{ route('documentos.conteudo', ['id' => $documento->id]) }}">{{ $documento->nome }}</a>
-                                @else
-                                    {{ $documento->nome }}
-                                @endif
-                            </td>
-                            <td>{{ $documento->nome_usuario }}</td>
-                            <td>{{ $permissoes }}</td>
-                            <td>{{ $conteudo }}</td>
-                            <td>{{ $dataCriacao }}</td>
-                            <td>
-                                @if($proprietario || ($permissoes == 'excluir'))
-                                    <a class="acoes-excluir" href="{{ route('documentos.excluir', ['id' => $documento->id]) }}">Excluir</a>
-                                @endif
-                                @if($proprietario)
-                                    <form action="{{ route('documentos.compartilhar') }}" method="POST">
-                                        @csrf
-                                        <input type="password" id="documento_id" name="documento_id" hidden value="{{ $documento->id ?? 0 }}">
-                                        <select name="usuario_id">
-                                            <option value="">Selecionar usuário</option>
-                                            @foreach ($usuarios as $usuario)
-                                                <option value="{{ $usuario->id }}">{{ $usuario->name }}</option>
-                                            @endforeach
-                                        </select>
-                                        <select name="permissao">
-                                            <option value="visualizar">Visualizar</option>
-                                            @if($documento->extensao == 'txt')
-                                                <option value="editar">Editar</option>
-                                            @endif
-                                            <option value="excluir">Excluir</option>
-                                        </select>
-                                        <button type="submit">Compartilhar</button>
-                                    </form>
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                </td>
+                                <td class="quebra-texto">{{ $documento->nome_usuario }}</td>
+                                <td>{{ $permissoes }}</td>
+                                <td class="quebra-texto">{{ $conteudo }}</td>
+                                <td>{{ $dataCriacao }}</td>
+                                <td>
+                                    @if($proprietario || ($permissoes == 'excluir'))
+                                        <a class="acoes-excluir" href="{{ route('documentos.excluir', ['id' => $documento->id]) }}">Excluir</a>
+                                    @endif
+                                    @if($proprietario)
+                                        <form action="{{ route('documentos.compartilhar') }}" method="POST">
+                                            @csrf
+                                            <input type="password" id="documento_id" name="documento_id" hidden value="{{ $documento->id ?? 0 }}">
+                                            <select name="usuario_id">
+                                                <option value="">Selecionar usuário</option>
+                                                @foreach ($usuarios as $usuario)
+                                                    <option value="{{ $usuario->id }}">{{ $usuario->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            <select name="permissao">
+                                                <option value="visualizar">Visualizar</option>
+                                                @if($documento->extensao == 'txt')
+                                                    <option value="editar">Editar</option>
+                                                @endif
+                                                <option value="excluir">Excluir</option>
+                                            </select>
+                                            <button type="submit">Compartilhar</button>
+                                        </form>
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         @endif
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             @csrf
